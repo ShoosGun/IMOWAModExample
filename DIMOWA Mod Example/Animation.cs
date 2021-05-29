667f99e6871d90d41193ec34dc09b8dc43bd01d3
ShoosGun <ivanrwpf@gmail.com>
2021-05-29 10:17:41 -0300

    coisos

---
+++ DIMOWA Mod Example/Animation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using CAMOWA.FBXRuntimeImporter;
+using CAMOWA.FBXRuntimeImporter.AnimationRead;
+
+namespace DIMOWA_Mod_Example
+{
+    public class FunnyAnimation : MonoBehaviour
+    {
+        AnimationClip clip;
+        Animator animator;
+        AnimationCurve positionXCurve;
+        public void Start()//ficar parente do "RocketScientist"
+        {
+            FBXFileParser fBXFile = new FBXFileParser("heartianVibe.fbx");
+            FBXAnimation[] animations = fBXFile.ReadAnimations();
+            positionXCurve = FBXCurveToUnityCurve(animations[0].BonesAnimation[0].PositionCurves[0]);
+            //AnimationCurve positionYCurve = FBXCurveToUnityCurve(animations[0].BonesAnimation[0].PositionCurves[1]);
+            //AnimationCurve positionZCurve = FBXCurveToUnityCurve(animations[0].BonesAnimation[0].PositionCurves[2]);
+
+            clip = new AnimationClip();
+            clip.SetCurve(""/*FrontierVillager_rig:Root_JNT/FrontierVillager_rig:Spine1_JNT*/, typeof(Transform), "localPosition.x", positionXCurve);
+            //clip.SetCurve("RocketScientist/FrontierVillager_default_idle/FrontierVillager_rig:Root_JNT/FrontierVillager_rig:Spine1_JNT", typeof(Transform), "localPosition.y", positionYCurve);
+            //clip.SetCurve("RocketScientist/FrontierVillager_default_idle/FrontierVillager_rig:Root_JNT/FrontierVillager_rig:Spine1_JNT", typeof(Transform), "localPosition.z", positionZCurve);
+            //clip.name = "Funny Animation";
+            //clip.wrapMode = WrapMode.Loop;
+
+            animator = gameObject.AddComponent<Animator>();//Como assim Animator não existe??????
+            animator.animation.AddClip(clip, clip.name);
+        }
+        bool isItPlaying = false;
+        public void Update()
+        {
+            //float f = positionXCurve.Evaluate(Time.time % positionXCurve[positionXCurve.length - 1].time);
+            //transform.localPosition = new Vector3(f, 0f, 0f);
+            if (!isItPlaying)
+            {
+                animator.animation[clip.name].layer = 123;
+                bool b = animator.animation.Play(clip.name);
+                Debug.Log($"is playing? {b}");
+                isItPlaying = true;
+                Debug.Log("length= " + clip.length);
+                Debug.Log("name= " + clip.name);
+            }
+        }
+        public static AnimationCurve FBXCurveToUnityCurve(FBXAnimationCurve fbxCurve)
+        {
+            FBXKeyFrame[] fbxKeyFrames = fbxCurve.KeyFrames;
+            Keyframe[] keyframes = new Keyframe[fbxKeyFrames.Length + 1];
+            keyframes[0] = new Keyframe(0f, fbxCurve.DefaultValue);
+            for (int i = 1; i < keyframes.Length; i++)
+                keyframes[i] = new Keyframe(fbxKeyFrames[i - 1].TimeInSeconds, fbxKeyFrames[i - 1].Value);
+
+            return new AnimationCurve(keyframes);
+        }
+
+    }
+}
---
+++ DIMOWA Mod Example/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CAMOWA;
+using UnityEngine;
+
+namespace DIMOWA_Mod_Example
+{
+    public class Gun
+    {
+        [IMOWAModInnit("Gun", 1/*Solar System*/, 2)]
+        public static void ModInnit(string startingPoint)
+        {
+            //1 - Import .obj (the gun mesh)
+            ObjImporter importer = new ObjImporter();
+            Mesh gunMesh = importer.ImportFile(@"Gun\gun.obj");
+
+            //2 - Find the stick
+            // 1 - stick(StickAndTherm/polySurface3)  ;  2 - small stick(StickAndTherm/polySurface2)  ; 3 - even smaller stick(StickAndTherm/polySurface4)
+            GameObject.Find("StickAndTherm/polySurface2").GetComponent<MeshRenderer>().enabled = false; // (Small) Stick goes puff
+            GameObject.Find("StickAndTherm/polySurface4").GetComponent<MeshRenderer>().enabled = false; // (Small) Stick goes puff
+            //3 - Change the stick to a gun
+            GameObject.Find("StickAndTherm/polySurface3").GetComponent<MeshFilter>().mesh = gunMesh;
+
+            GameObject.Find("RocketScientist/FrontierVillager_default_idle").AddComponent<FunnyAnimation>();
+        }
+
+    }
+}
